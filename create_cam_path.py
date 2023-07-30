785c388b4877156c907efffe4d5873a7fe900f3c
alaa-khamis <khamisalaag@gmail.com>
2023-07-30 15:31:35 +0300

    second try

--- create_cam_path.py
+++ create_cam_path.py
@@ -6,21 +6,46 @@ def load_transforms_json(json_path):
         data = json.load(f)
     return data
 
+def compute_difference(matrix1, matrix2):
+    difference = 0
+    for i in range(len(matrix1)):
+        for j in range(len(matrix1[0])):
+            difference += abs(matrix1[i][j] - matrix2[i][j])
+    return difference
+
 def create_camera_path(transforms_data, fov, aspect, render_height, render_width, duration, smoothness):
     keyframes = []
     camera_path = []
     
-    keyframes_num = 5
-    for i in range(keyframes_num):
+    differences = []
+    previous_matrix = transforms_data["frames"][0]["transform_matrix"]
+
+    for i, frame in enumerate(transforms_data["frames"]):
+        matrix = frame["transform_matrix"]
+        difference = compute_difference(previous_matrix, matrix)
+        differences.append((difference, i))  # Store the difference along with the frame index
+        previous_matrix = matrix
+    
+    # Sort differences in decreasing order and select the frame indices corresponding to the largest differences
+    differences.sort(reverse=True)
+    keyframes_num = max(duration // 3, 5)
+    selected_indices = sorted([index for _, index in differences[:keyframes_num]])
+
+    # Ensure that the first frame is always included
+    if 0 not in selected_indices:
+        selected_indices.pop()  # Remove the last index
+        selected_indices.append(0)  # Add the first index
+        selected_indices.sort()  # Sort again to maintain ascending order
+
+    for i in selected_indices:
         frame = transforms_data["frames"][i]
         matrix = frame["transform_matrix"]
         flat_matrix = [item for sublist in matrix for item in sublist]
-        
 
         properties = [
             ["FOV", fov],
             ["NAME", f"Camera {i}"],
-            ["TIME", i/(keyframes_num - 1)]  # Assuming equal time intervals
+            ["TIME", i / (len(transforms_data["frames"]) - 1)]  # Assuming equal time intervals
         ]
         keyframe = {
             "matrix": str(flat_matrix),
